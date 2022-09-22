7e43eb14d7fd315ee0b32dbb6805d446c29b4e0b
JavierECS <javier.cuellar@accenture.com>
2022-09-22 17:38:32 -0500

    veamos que pedo

--- src/app/products/details-product/details-product.component.ts
+++ src/app/products/details-product/details-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../shared/data.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-details-product',
@@ -10,13 +11,21 @@ import { DataService } from '../../shared/data.service';
 export class DetailsProductComponent implements OnInit {
   public product:any = {}
   
-  constructor(private route:ActivatedRoute, private dataSvc: DataService) { }
+  constructor(
+    private route:ActivatedRoute, 
+    private dataSvc: DataService, 
+    private location: Location
+    )  { }
 
   ngOnInit(): void {
     const productID = this.route.snapshot.paramMap.get('id');
     console.log('ProductID', productID );
     [this.product] = this.dataSvc.getProductById(productID);
 
+  }
+
+  onGoBack(): void{
+    this.location.back();
   }
 
 }
